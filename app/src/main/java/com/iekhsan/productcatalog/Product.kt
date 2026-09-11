42b6a3812908edecc0709d57924cb49383f0a327
Iekhsan <iekhsanmuhazzib@gmail.com>
2026-09-11 16:24:55 +0800

    feat: add Product data class

---
+++ app/src/main/java/com/iekhsan/productcatalog/Product.kt
@@ -0,0 +1,11 @@
+package com.iekhsan.productcatalog
+
+data class Product(
+    val id: Int,
+    val title: String,
+    val thumbnail: String,
+    val description: String,
+    val price: Double,
+    val rating: Double,
+    val images: List<String>
+)
