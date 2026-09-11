b6a331031eaacb6a0dbd399665856ef8d7ca4ee3
Iekhsan <iekhsanmuhazzib@gmail.com>
2026-09-11 18:26:18 +0800

    chore: add Retrofit and OkHttp dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.okhttp.logging)
 }
