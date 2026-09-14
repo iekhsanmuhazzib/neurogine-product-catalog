a48a71bf13a8052d6f92f6a0df583cfb41531352
Iekhsan <iekhsanmuhazzib@gmail.com>
2026-09-14 15:21:39 +0800

    feat: wire ProductListScreen into MainActivity

--- app/src/main/java/com/iekhsan/productcatalog/MainActivity.kt
+++ app/src/main/java/com/iekhsan/productcatalog/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.iekhsan.productcatalog.ui.theme.ProductCatalogTheme
+import com.iekhsan.productcatalog.ui.productlist.ProductListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ProductCatalogTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ProductListScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProductCatalogTheme {
-        Greeting("Android")
     }
 }
